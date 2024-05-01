28c4be1e6ec68693afaf89a0efc01fe2cbafee05
Kale Kundert <kale@thekunderts.net>
2024-05-01 15:52:38 -0400

    feat: automatically import the pymol module

--- macromol_dataframe/__init__.py
+++ macromol_dataframe/__init__.py
@@ -5,6 +5,7 @@ Manipulate macromolecular coordinate data using data frames.
 __version__ = '0.1.0'
 
 from .mmcif import *
+from .pymol import *
 from .atoms import *
 from .coords import *
 from .error import *
